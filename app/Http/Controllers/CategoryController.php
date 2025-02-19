07458fb22a51697b2b429659dd43e404fcec8842
GE08T <gaerthamgk@gmail.com>
2025-02-19 20:28:23 +0700

    Membuat routing ke view

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function foodBeverage() {
+        return view('category/food-beverage');
+    }
+
+    public function beautyHealth() {
+        return view('category/beauty-health');
+    }
+
+    public function homeCare() {
+        return view('category/home-care');
+    }
+
+    public function babyKids() {
+        return view('category/baby-kids');
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function profile($id, $name) {
+        return view('profile', ['id' => $id, 'name' => $name]);
+    }
 }
